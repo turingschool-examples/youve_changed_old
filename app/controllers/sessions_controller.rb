3f8aed6f44985ff38ef7be461e1db8e870832e39
Josh Mejia <joshua.mejia@gmail.com>
2015-02-23 20:50:02 -0700

    Implement user login and signup.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+end
