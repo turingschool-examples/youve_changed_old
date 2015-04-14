ebabea43d504173e8101fde8feab0297d8fc1262
Josh Mejia <joshua.mejia@gmail.com>
2015-04-13 22:54:04 -0600

    Prepare for mailer and worker class

--- config/application.rb
+++ config/application.rb
@@ -8,24 +8,7 @@ Bundler.require(*Rails.groups)
 
 module YouveChanged
   class Application < Rails::Application
-    config.action_mailer.delivery_method = :smtp
-
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.mandrillapp.com',
-      port:                 '587',
-      domain:               'blogger.com',
-      user_name:            'joshua.mejia@gmail.com',
-      password:             'kYSAGWvOxPPbbZvk0_yYig',
-      authentication:       'plain',
-      enable_starttls_auto: true
-    }
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.active_job.queue_adapter = :sidekiq
-
-    config.autoload_paths += %W(
-      #{config.root}/app/jobs
-    )
   end
 end
