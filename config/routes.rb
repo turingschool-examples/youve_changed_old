0027f2ab22ce5e8fbef165aff555cf019209b778
worace <horace.d.williams@gmail.com>
2015-07-09 08:52:17 -0600

    removing some more sidekiq stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
   root to: "welcome#index"
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
