7179af3d8207bb78a28c9d01c69ef30caa40ac65
worace <horace.d.williams@gmail.com>
2015-07-09 13:57:30 -0600

    prompt for username

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module YouveChanged
       address:              'smtp.mandrillapp.com',
       port:                 '587',
       domain:               'blogger.com',
-      user_name:            "horace.d.williams@gmail.com",
-      password:             "qtCjX4zEBIioiB8ObuO_0A",
+      user_name:            YOUR_USER_NAME,
+      password:             YOUR_PASSWORD,
       authentication:       'plain',
       enable_starttls_auto: true
     }
